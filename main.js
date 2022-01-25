058bccfe7227cedc9bc27f65e9440d2022308a30
Jiya-Garg <69499533+Jiya-Garg@users.noreply.github.com>
2022-01-25 20:04:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function bestsellor1() {
+    window.location = "index1.html";
+}
+function bestsellor2() {
+    window.location = "index2.html";
+}
+function bestsellor3() {
+    window.location = "index3.html";
+}
+function back() {
+    window.location = "index.html"
+}
